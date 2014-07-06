7af8b8d88703225d717bfa61a9051ff14b0fab89
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-07-07 02:53:25 +0800

    requirejs is working fine I think

--- scripts/modules/todo/todoView.js
+++ scripts/modules/todo/todoView.js
@@ -7,26 +7,46 @@ define([
   ], function(
 
     Backbone,
-    _,
+    _
 
     ){
+
+  //testing model
+  var todos = {title: "some title", description: "some des"};
 
   console.log("this is the todo View");
   var TodoView = Backbone.View.extend({
     el: '#container2',
-    // template: _.template( $("#todo-list-template").html()),
+    template: _.template( $("#todo-list-template").html()),
     initialize: function(){
       this.render();
       console.log("todoView initialize....");
     },
     render: function(){
       console.log("rendering....");
-      var template= _.template( $("#todo-list-template").html());
-      this.$el.html(template;
+      console.log("todos.title: "+todos.title);
+      this.$el.html(this.template(todos));
       return this;
     }
   });
+
+  var TodoView2 = Backbone.View.extend({
+    el: '#todo-list',
+    tagName: 'li',
+    className: '.todo-list-item',
+    initialize: function(){
+      this.render();
+      console.log("Finished rendering TodoView2. ");
+    },
+    render: function(){
+      console.log("rendering TodoView2...");
+      this.$el.html("MIGHT BE A LIST ITEM");
+      return this;
+    }
+  });
+
   var todoView = new TodoView();
+  var todoView2 = new TodoView2();
 })
 
 
